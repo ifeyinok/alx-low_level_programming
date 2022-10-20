2b0d1a18a627379761463d7a0149e6cf338492be
“ifeyinok” <okoliifeyinwa11@gmail.com>
2022-10-20 07:06:45 -0700

    Task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,42 +1,29 @@
-#include <stdlib.h>
-#include <string.h>
 #include "lists.h"
-
 /**
-  * *add_node - adds a new node at the beginning of a list_t list.
-  * @head: pointer to first elements.
-  * @str: string.
-  * Return: the address of the new elements,
-  * or NULL if it failed.
-  */
+ * add_node - adds a new node at the beginning
+ * of a list_t list.
+ * @head: head of the linked list.
+ * @str: string to store in the list.
+ * Return: address of the head.
+ */
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *newNode;
+	list_t *new_node;
+	size_t n;
 
-	newNode = (list_t *)malloc(sizeof(list_t));
-	if (newNode == NULL)
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
 		return (NULL);
-	newNode->str = strdup(str);
-	newNode->len = string_length(str);
-	newNode->next = *head;
-	*head = newNode;
+
+	new_node->str = strdup(str);
+
+	for (n = 0; str[n]; n++)
+		;
+
+	new_node->len = n;
+	new_node->next = *head;
+	*head = new_node;
 
 	return (*head);
 }
-/**
-  * string_length - finds the length of a string.
-  * Return: length of c.
-  * @pointer: pointer.
-  */
-int string_length(const char *pointer)
-{
-	int c = 0;
-
-	while (*(pointer + c) != '\0')
-	{
-		c++;
-	}
-	return (c);
-}
-
